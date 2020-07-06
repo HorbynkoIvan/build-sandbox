c87cb55dac8ce7e59402b60c1e00bdf15e11b149
HorbynkoIvan <horbynko.i@gmail.com>
2020-07-06 18:42:56 +0300

    work with babel plugins

---
+++ src/main.js
@@ -0,0 +1,14 @@
+class App {
+    constructor() {
+        this.run = (name="World")=>{
+            console.log(`Hello ${name}`)
+        }
+    }
+    /*run() {
+        const name = "World"
+        console.log(`Hello ${name}`)
+    }*/
+}
+
+const app = new App()
+app.run()
