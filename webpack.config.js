608ee724da849daef697a2892effec10f5e6f871
HorbynkoIvan <horbynko.i@gmail.com>
2020-07-15 21:41:25 +0300

    works with loaders

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    mode: "development",
+
+    module: {
+        rules: [
+            {
+                test: /\.png$/,
+                use: [{
+                    loader: "file-loader",
+                    options: {
+                        outputPath: 'images',
+                        name: '[name]-[sha1:hash:7].[ext]'
+                    }
+                }]
+            }
+        ]
+    }
+}
